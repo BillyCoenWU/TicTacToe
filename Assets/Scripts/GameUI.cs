42530a5d6f09e938053b0e6991715d64a759889f
Romulo Gomes <romulomarquessantos@hotmail.com>
2020-03-03 21:36:04 -0300

    TicTacToe New Version Commit

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,109 @@
+﻿namespace IBMR
+{
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using System.Collections;
+
+    public class GameUI : MonoBehaviour
+    {
+        // velocidade da transição que abertura da barra
+        [SerializeField]
+        private float m_transitionSpeed = 1.0f;
+
+        // variavel para ter acesso a barra
+        // que é ativada ao fim do jogo
+        [SerializeField]
+        private RectTransform m_bar = null;
+
+        // texto que vai indicar se houve empate
+        // ou vitoria entre os jogadores
+        [SerializeField]
+        private TextMeshProUGUI m_text = null;
+
+        // botoes que vão se desativados ao fim do jogo
+        [SerializeField]
+        private GameObject[] m_gameObjects = null;
+
+        // botoes que seram ativados quando a transição acabar
+        [SerializeField]
+        private Button[] m_buttons = null;
+
+        [SerializeField]
+        private GameObject[] m_playersTurn = null;
+
+        // informação do tamanho da barra para quando ela estiver fechada
+        private readonly Vector3 CLOSED_SIZE = new Vector3(0.0f, 1.0f, 1.0f);
+        // informação do tamanho total da barra para quando ela terminar de abrir
+        private readonly Vector3 FULL_SIZE = Vector3.one;
+
+        // ativa e desativa o indicador de turno dos jogadores
+        public void SetPlayerTurn (int turn)
+        {
+            // passa por todos os indicadores
+            // setando true ou false em cada um
+            // de acordo com o valor do tuno atual
+            for (int i = 0; i < m_playersTurn.Length; i++)
+            {
+                m_playersTurn[i].SetActive(turn == i);
+            }
+        }
+
+        // metodo para setar as informações na tela de fim de jogo e chamar a transição da UI
+        public void OpenUI (CONDITION finalGameCondition, int player)
+        {
+            // desativa os indicadores de turnos de ambos os jogadores
+            // já que passa um valor que não é referente a turno nenhum
+            SetPlayerTurn(-1);
+
+            // coloca no texto da barra de final de jogo se os jogadores empataram ou qual jogador ganhou
+            m_text.text = finalGameCondition == CONDITION.DRAW ? "EMPATE!" : $"VITÓRIA DO JOGADOR {player}!";
+
+            // faz cada botao que ainda não desativou ficar desatiado
+            foreach (Button b in m_buttons)
+            {
+                b.interactable = false;
+            }
+
+            // inicia a transição que vai abrir a tela de fim de jogo
+            StartCoroutine(OpenTransition());
+        }
+
+        // Coroutina que vai abrir a tela quando acaba o jogo
+        private IEnumerator OpenTransition ()
+        {
+            // variavel de tempo decorrido, em segundos
+            // 1.0f = 1 segundo
+            float elapsedTime = 0.0f;
+
+            // enquanto o valor do tempo decorrido for menor que 1.0f
+            while (elapsedTime < 1.0f)
+            {
+                // soma na variavel de tempo decorrido
+                // o intervalo entre frames multiplicado
+                // pela velocidade da transição
+                elapsedTime += Time.deltaTime * m_transitionSpeed;
+
+                // usa do metodo Lerp da estrutura Vector3 para
+                // fazer uma transição do tamanho da tela
+                // de quando ela está fechada para quando ela está aberta
+                m_bar.localScale = Vector3.Lerp(CLOSED_SIZE, FULL_SIZE, elapsedTime);
+
+                //indica que esse recurso deve esperar até o próximo frame para continuar
+                yield return null;
+            }
+
+            // quando termina a transição:
+
+            // ativa o texto
+            m_text.gameObject.SetActive(true);
+
+            // ativa os botões de resetar ou fechar o jogo
+            foreach (GameObject go in m_gameObjects)
+            {
+                // ativa o GameObject atualda lista de botões
+                go.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TicTacToe.cs
@@ -0,0 +1,223 @@
+﻿namespace IBMR
+{
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+    // informações de possiveis
+    // condições do jogo
+    public enum CONDITION
+    {
+        CHANGE_TURN = 0,
+        WIN,
+        DRAW
+    }
+
+    public class TicTacToe : MonoBehaviour
+    {
+        // código que controla a tela do fim de jogo
+        [SerializeField]
+        private GameUI m_ui = null;
+
+        // turno que indica qual jogador deve jogar agora
+        private int m_turn = 0;
+
+        // infromacao apenas para o sistema saber o tamanho maximo do board
+        private const int BOARD_SIZE = 3;
+
+        // valor do simbolo do jogador 1 que será desenhado no board
+        private const string PLAYER_ONE_SYMBOL = "X";
+        // valor do simbolo do jogador 2 que será desenhado no board
+        private const string PLAYER_TWO_SYMBOL = "0";
+
+        // cor do simbolo do jogador 1 -> vermelho
+        private readonly Color PLAYER_ONE_COLOR = Color.red;
+        // cor do simbolo do jogador 2 -> azul
+        private readonly Color PLAYER_TWO_COLOR = Color.blue;
+
+        // pega a cor do jogador de acordo com o valor do turno
+        private Color GetPlayerColor => m_turn == 0 ? PLAYER_ONE_COLOR : PLAYER_TWO_COLOR;
+
+        // pega o simbolo do jogador de acordo com o valor do turno
+        private string GetPlayerSymbol => m_turn == 0 ? PLAYER_ONE_SYMBOL : PLAYER_TWO_SYMBOL;
+
+        // matrix de duas dimensões que informa o estado inicial do board
+        // sera alterada pelo sistema e usada para verificar as condições de
+        // vitoria ou empate
+        private int[,] m_board = new int[BOARD_SIZE, BOARD_SIZE] {  { -1, -1, -1 },
+                                                                    { -1, -1, -1 },
+                                                                    { -1, -1, -1 }};
+
+        // textos dos campos que os jogadores selecionam
+        [SerializeField]
+        private TextMeshProUGUI[] m_texts = null;
+
+        // metodo que é chamado quando um dos botões é clicado
+        // ele recebe o id do botão
+        public void OnClickButton (int buttonID)
+        {
+            // seta na informaçaõ do sistema qual campo foi marcado pro quando jogador
+            m_board[GetRoll(buttonID), GetColumn(buttonID)] = m_turn;
+
+            // seta o simbolo do jogador no campo
+            m_texts[buttonID].text = GetPlayerSymbol;
+            // seta a cor no texto do simbolo do jogador
+            m_texts[buttonID].color = GetPlayerColor;
+
+            // recebe a condição atual do jogo que foi verificada
+            CONDITION currentCondition = GameCondition(buttonID);
+
+            // de acordo com a condição atual do jogo
+            switch (currentCondition)
+            {
+                // caso seja uma vitoria ou um empate
+                case CONDITION.DRAW:
+                case CONDITION.WIN:
+                    // abre a tela do fim de jogo
+                    m_ui.OpenUI(currentCondition, m_turn + 1);
+                    break;
+
+                    // caso seja para trocar de turno
+                case CONDITION.CHANGE_TURN:
+                    // troca qual jogador deve jogar agora
+                    m_turn = m_turn == 0 ? 1 : 0;
+                    m_ui.SetPlayerTurn(m_turn);
+                    break;
+            }
+        }
+
+        // metodo que de acordo com o id do botão
+        // pega em qual linha os jogadores clicaram
+        private int GetRoll (int index) => (index / BOARD_SIZE);
+
+        // metodo que de acordo com o id do botão
+        // pega em qual coluna os jogadores clicaram
+        private int GetColumn (int index) => (index % BOARD_SIZE);
+
+        // metodo que de acordo com o id do botão
+        // fala se o jogador clicou em uma das casas
+        // que formam uma diagonal
+        private bool IsDiagonal (int index) => (index % 2 == 0);
+
+        // metodo que verifica se um dos jogadores ganhou a partida
+        // ou se houve um empate
+        private CONDITION GameCondition (int index)
+        {
+            // verifica se a coluna em que o jogador atual
+            // selecionou um dos campos forma uma sequencia
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                if (m_board[x, GetColumn(index)] != m_turn)
+                {
+                    break;
+                }
+
+                if (x == 2)
+                {
+                    return CONDITION.WIN;
+                }
+            }
+
+            // verifica se a linha em que o jogador atual
+            // selecionou um dos campos forma uma sequencia
+            for (int y = 0; y < BOARD_SIZE; y++)
+            {
+                if (m_board[GetRoll(index), y] != m_turn)
+                {
+                    break;
+                }
+
+                if (y == 2)
+                {
+                    return CONDITION.WIN;
+                }
+            }
+
+            // caso seja uma das casas na diagonal
+            if (IsDiagonal(index))
+            {
+                // verifica a primeira diagonal
+                for (int i = 0; i < BOARD_SIZE; i++)
+                {
+                    if (m_board[i, i] != m_turn)
+                    {
+                        break;
+                    }
+
+                    if (i == 2)
+                    {
+                        return CONDITION.WIN;
+                    }
+                }
+
+                // verifica a segunda diagonal
+                for (int i = 0; i < BOARD_SIZE; i++)
+                {
+                    if (m_board[i, (2-i)] != m_turn)
+                    {
+                        break;
+                    }
+
+                    if (i == 2)
+                    {
+                        return CONDITION.WIN;
+                    }
+                }
+            }
+
+            bool breakAll = false;
+
+            // caso não tenha ocorrido uma vitória
+            // verifica se houve empate
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                for (int y = 0; y < BOARD_SIZE; y++)
+                {
+                    // faz uma verificação em cada uma das 9 casas
+                    // para ver se ainda tem algum campo que não foi escolhido
+                    if (m_board[x, y] == -1)
+                    {
+                        // caso ainda haja campos para escolher para a verificação
+                        breakAll = true;
+                        break;
+                    }
+                }
+
+                if (breakAll)
+                {
+                    break;
+                }
+
+                if (x == 2)
+                {
+                    return CONDITION.DRAW;
+                }
+            }
+            
+            // caso n]ao tenha ocorrido empate
+            // então troca o turno dos jogadores
+            return CONDITION.CHANGE_TURN;
+        }
+
+        // Metodo responsável por fechar o jogo
+        public void CloseGame ()
+        {
+            // caso esteja sendo executando dentro da Unity
+#if UNITY_EDITOR
+            // só faz a ferramente parar de simular o jogo
+            EditorApplication.isPlaying = false;
+#else
+            // caso esteja sendo executado na build
+            // fechar a aplicação
+            Application.Quit();
+#endif
+        }
+
+        // Metodo responsável por recarregar a cena e resetar o jogo
+        public void ReloadScene () => SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+}
